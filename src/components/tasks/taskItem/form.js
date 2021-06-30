23d4b5574931215c7c64a32bba4cf7c84e4892f4
unknown <juliesg@tut.by>
2021-07-01 01:59:51 +0300

    modal

--- src/components/tasks/taskItem/form.js
+++ src/components/tasks/taskItem/form.js
@@ -24,12 +24,19 @@ export default function TodosForm(props) {
   };
 
   return (
-    <div>
-      <form className={style.form} onSubmit={handleSubmit} ref={form}>
+    <div className={style.form}>
+      <form className={style.content} onSubmit={handleSubmit} ref={form}>
         <h2>Add new task</h2>
         <input type="text" name="title" placeholder="Title" />
         <input type="datetime-local" name="dead_line" placeholder="Deadline" />
         <textarea name="description" placeholder="Description" />
+        <div className={style.close}></div>
+        <div className={style.radio}>
+          <input type="radio" id="Undone" name="completed" value="0" />
+          <label htmlFor="Undone">Undone</label>
+          <input type="radio" id="Done" name="completed" value="1" />
+          <label htmlFor="Done">Done</label>
+        </div>
         <input type="submit" value="Add" className={style.add} />
       </form>
     </div>
